21bcddd3d58d49a7cfb1cc4dc50ce0556739943a
dasdawd <dawasdw>
2026-02-14 14:37:03 +0500

    Save

---
+++ WebApplication1/Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data.Interfaces;
+
+namespace WebApplication1.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategores IAllCategores;
+        public ItemsController(IItems IAllItems, ICategores IAllCategores)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategores = IAllCategores;
+        }
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}
