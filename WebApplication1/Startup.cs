3cc4eda295dd2e48fd9213b959930948cf58738f
dasdawd <dawasdw>
2026-02-14 13:34:45 +0500

    Разработанная структура проекта коробкин

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
