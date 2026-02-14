b892d3323ea09e09a206deb7f96519e3d9ecb519
dasdawd <dawasdw>
2026-02-14 15:04:35 +0500

    Разработанное практическое занятие на оценку удовлетворительно. Коробкин

--- WebApplication1/Data/Mocks/MockItems.cs
+++ WebApplication1/Data/Mocks/MockItems.cs
@@ -2,6 +2,7 @@
 using WebApplication1.Data.Models;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace WebApplication1.Data.Mocks
 {
@@ -18,39 +19,39 @@ namespace WebApplication1.Data.Mocks
                         Id = 0,
                         Name = "DEXP DK-2200D",
                         Description = "Благодаря черному корпусу работает лучше",
-                        img = "https://www.dns-shop.ru/product/74815b45b14f1b29/elektrocajnik-dexp-dk-2200d-cernyj/",
+                        img = "https://c.dns-shop.ru/thumb/st1/fit/320/250/0a2aa88619a86c403eb236779d398bd5/13af31a9ab99756ef17f59d69553592c3c0f584df4e9377627901d871a9b720a.jpg",
                         Price = 6000,
-                        category = _category.AllCategorys.Where(x => x.Id == 0).First()
+                        category = _category.AllCategorys.Where(x => x.Id == 1).First()
                     },
                     new Items() {
-                        Id = 0,
+                        Id = 1,
                         Name = "Крутой чайник",
                         Description = "Благодаря черному корпусу работает лучше",
-                        img = "https://www.dns-shop.ru/product/74815b45b14f1b29/elektrocajnik-dexp-dk-2200d-cernyj/",
+                        img = "https://c.dns-shop.ru/thumb/st1/fit/320/250/0a2aa88619a86c403eb236779d398bd5/13af31a9ab99756ef17f59d69553592c3c0f584df4e9377627901d871a9b720a.jpg",
                         Price = 6000,
-                        category = _category.AllCategorys.Where(x => x.Id == 0).First()
+                        category = _category.AllCategorys.Where(x => x.Id == 1).First()
                     },
                     new Items() {
-                        Id = 0,
+                        Id = 2,
                         Name = "Некрутой чайник",
                         Description = "Благодаря черному корпусу работает лучше",
-                        img = "https://www.dns-shop.ru/product/74815b45b14f1b29/elektrocajnik-dexp-dk-2200d-cernyj/",
+                        img = "https://c.dns-shop.ru/thumb/st1/fit/320/250/0a2aa88619a86c403eb236779d398bd5/13af31a9ab99756ef17f59d69553592c3c0f584df4e9377627901d871a9b720a.jpg",
                         Price = 6000,
-                        category = _category.AllCategorys.Where(x => x.Id == 0).First()
+                        category = _category.AllCategorys.Where(x => x.Id == 1).First()
                     },
                     new Items() {
-                        Id = 0,
+                        Id = 3,
                         Name = "Чайник",
                         Description = "Благодаря черному корпусу работает лучше",
-                        img = "https://www.dns-shop.ru/product/74815b45b14f1b29/elektrocajnik-dexp-dk-2200d-cernyj/",
+                        img = "https://c.dns-shop.ru/thumb/st1/fit/320/250/0a2aa88619a86c403eb236779d398bd5/13af31a9ab99756ef17f59d69553592c3c0f584df4e9377627901d871a9b720a.jpg",
                         Price = 6000,
-                        category = _category.AllCategorys.Where(x => x.Id == 0).First()
+                        category = _category.AllCategorys.Where(x => x.Id == 1).First()
                     },
                     new Items() {
-                        Id = 0,
+                        Id = 4,
                         Name = "Не чайник",
                         Description = "Благодаря черному корпусу работает лучше",
-                        img = "https://www.dns-shop.ru/product/74815b45b14f1b29/elektrocajnik-dexp-dk-2200d-cernyj/",
+                        img = "https://c.dns-shop.ru/thumb/st4/fit/320/250/719c3a1544acc574f4f03ca2bf1f2d6a/41c5b8c3c552a0e4a0af35916ceb8d43bc2ec6e982b3212eeefad4d099314bef.jpg",
                         Price = 6000,
                         category = _category.AllCategorys.Where(x => x.Id == 0).First()
                     },
