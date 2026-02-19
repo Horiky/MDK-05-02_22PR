8cf771308ef3ccb4853a8d94765409378b4788a8
фпырры <>
2026-02-19 12:53:36 +0500

    Разработанное подключение к MySql. Коробкин

---
+++ WebApplication1/Data/Common/Conection.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
+namespace WebApplication1.Data.Common
+{
+    public class Conection
+    {
+        readonly static string ConnectionData = "server=127.0.0.1;port=3306;database=23pr;uid=root;pwd=;";
+        public static MySqlConnection MySql0pen()
+        {
+            MySqlConnection NewMySqlConnection = new MySqlConnection(ConnectionData);
+            NewMySqlConnection.Open();
+            return NewMySqlConnection;
+        }
+
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection Connection)
+        {
+            MySqlCommand NewMySqlCommand = new MySqlCommand(Query, Connection);
+            return NewMySqlCommand.ExecuteReader();
+        }
+
+        public static void MySqlClose(MySqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
